f604162c522ca9a372f6f9af09e904ab05b06502
Akshay Lavhagale <akshay.lavhagale27@gmail.com>
2020-02-18 16:04:53 -0500

    Add files via upload

---
+++ HW04a_AkshayLavhagale_Test.py
@@ -0,0 +1,25 @@
+''' Akshay Lavhagale
+    Homework 04a - Develop with the Perspective of the Tester in mind '''
+
+import unittest
+import HW04a_AkshayLavhagale
+
+
+class TestMain(unittest.TestCase):
+    def test_userid(self):
+        # Testing whether the userid is correct or no
+        self.assertEqual(HW04a_AkshayLavhagale.github_handle('AkshayLavhgale'), "The user ID does not exist")
+
+    def test_get_total_commit(self):
+        # Testing the number of commits for repositories
+        self.assertEqual(HW04a_AkshayLavhagale.github_handle('AkshayLavhagale'), {'Fractions': 1, 'helloworld': 1,
+                                                                                  'knn': 1, 'K_Means_Clustering': 1,
+                                                                                  'Linear-Regression': 1, 'SE_540': 1,
+                                                                                  'svm': 1, 'SW567_GitHub': 3,
+                                                                                  'SW_567_HW_01': 2,
+                                                                                  'Testing': 1, 'Triangle567': 5})
+
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
